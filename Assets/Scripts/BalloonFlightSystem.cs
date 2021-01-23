9fabc592af5b2c3c60e886b3c7303a60070054d9
Steven <steven@properchaos.nl>
2021-01-23 19:59:07 +0100

    Add crashing sound

--- Assets/Scripts/BalloonFlightSystem.cs
+++ Assets/Scripts/BalloonFlightSystem.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BalloonFlightSystem : MonoBehaviour
 {
     private Rigidbody rigidbody;
+
+    public AudioSource destructionSound;
     public float heat = 1.0f;
     public Vector2 wind = new Vector2(0, 1);
 
@@ -12,15 +14,21 @@ public class BalloonFlightSystem : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        destructionSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         // Reduce heat
-        heat -= 0.001f;
+        heat -= 0.005f;
 
         // Change position
         rigidbody.velocity = new Vector3(wind.x, heat, wind.y);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        destructionSound.Play();
     }
 }

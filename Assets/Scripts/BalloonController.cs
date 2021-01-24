b5ff5cc7d9d0e5ea27df5966fa02c96799ca141b
Joris van Vugt <jorisvanvugt@gmail.com>
2021-01-24 10:47:11 +0100

    Make Handle Controller

--- Assets/Scripts/BalloonController.cs
+++ Assets/Scripts/BalloonController.cs
@@ -4,18 +4,29 @@ using UnityEngine.SceneManagement;
 public class BalloonController : MonoBehaviour
 {
     private BalloonFlightSystem balloon;
-    private bool throttleActive = false;
+    public bool throttleActive = false;
+    private ParticleSystem ps;
 
     // Start is called before the first frame update
     void Start()
     {
         balloon = GetComponent<BalloonFlightSystem>();
+        ps = GetComponentInChildren<ParticleSystem>();
     }
     private void FixedUpdate()
     {
         if (throttleActive) {
             balloon.heat = Mathf.Clamp(balloon.heat + 0.05f, 20, 150);
+            if (!ps.isPlaying)
+            {
+                ps.Play();
+            }
         }
+        else
+        {
+            ps.Stop();
+        }
+
     }
 
     private void OnReset()
